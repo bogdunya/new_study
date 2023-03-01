1c9fc0076d61c7874a9fe2678ae168189a27092a
Shvidky Bogdan <bogdunya@mail.ru>
2023-03-01 20:18:36 +0300

    First commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+	echo "NEW STR FOR GIT";
+
+	echo "Hello World";
+	echo "Hi";
+	echo "World";
+	echo "WEEEEE";
+	echo "Hello World";
+	echo "Hi";
+	echo "World";
+	echo "WEEEEE";echo "Hello World";
+	echo "Hi";
+	echo "World";
+	echo "WEEEEE";
+	
+	
+	echo "new feature";
+	#New str
